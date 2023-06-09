80e1a211d004ddf68804cffbe8df0bf42b2c1ed7
Abhinav Tushar <abhinav@lepisma.xyz>
2023-06-10 00:51:49 +0530

    Add dummy list view

---
+++ app/src/main/java/com/example/pile/Data.kt
@@ -0,0 +1,7 @@
+package com.example.pile
+
+data class OrgNode(val title: String, val path: String)
+
+fun readFilesFromRepository(): List<OrgNode> {
+    return listOf()
+}
--- app/src/main/java/com/example/pile/MainActivity.kt
+++ app/src/main/java/com/example/pile/MainActivity.kt
@@ -1,18 +1,24 @@
 package com.example.pile
 
 import android.os.Bundle
+import android.os.CountDownTimer
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.pile.ui.theme.PileTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -22,25 +28,42 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Column (
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Bottom
+                    ) {
+                        OrgNodeList(nodes = listOf(OrgNode("Test", ""), OrgNode("Test B", "")))
+                        SearchCreateField()
+                    }
                 }
             }
         }
     }
 }
 
+@ExperimentalMaterial3Api
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun SearchCreateField() {
+    var text by remember { mutableStateOf("") }
+
+    Column {
+        OutlinedTextField(
+            value = text,
+            onValueChange = { text = it },
+            label = { Text(text = "Search or Create") },
+            placeholder = { Text(text = "Node name") }
+        )
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    PileTheme {
-        Greeting("Android")
+fun OrgNodeList(nodes: List<OrgNode>) {
+    LazyColumn {
+        items(nodes) { node -> OrgNodeItem(node) }
     }
+}
+
+@Composable
+fun OrgNodeItem(node: OrgNode) {
+    Text(node.title)
 }
