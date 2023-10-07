882ee8db96582d09f35c4f035b7035cb298c5503
Abhinav Tushar <abhinav@lepisma.xyz>
2023-10-07 10:51:07 +0530

    make node title gray

--- app/src/main/java/com/example/pile/ui/components/NodeScreen.kt
+++ app/src/main/java/com/example/pile/ui/components/NodeScreen.kt
@@ -22,6 +22,7 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.style.TextOverflow
@@ -44,7 +45,7 @@ fun NodeScreen(node: OrgNode, goBack: () -> Unit) {
                 TopAppBar(
                     colors = TopAppBarDefaults.smallTopAppBarColors(),
                     title = {
-                        Text(node.title, maxLines = 1, overflow = TextOverflow.Ellipsis)
+                        Text(node.title, color = Color.Gray, maxLines = 1, overflow = TextOverflow.Ellipsis)
                     },
                     navigationIcon = {
                         IconButton(onClick = { goBack() }) {
