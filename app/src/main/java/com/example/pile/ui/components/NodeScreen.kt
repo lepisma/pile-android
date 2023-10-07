619fe87062d76b2b76140e4a31c37386a9f2882d
Abhinav Tushar <abhinav@lepisma.xyz>
2023-10-07 13:38:31 +0530

    Add dummy edit mode view

--- app/src/main/java/com/example/pile/ui/components/NodeScreen.kt
+++ app/src/main/java/com/example/pile/ui/components/NodeScreen.kt
@@ -5,11 +5,14 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material.icons.filled.List
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
@@ -17,14 +20,21 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Switch
+import androidx.compose.material3.SwitchDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
@@ -33,11 +43,36 @@ import androidx.compose.ui.unit.sp
 import com.example.pile.OrgNode
 import com.example.pile.readOrgContent
 import com.example.pile.ui.theme.PileTheme
+import compose.icons.FontAwesomeIcons
+import compose.icons.fontawesomeicons.Solid
+import compose.icons.fontawesomeicons.solid.Glasses
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun NodeEdit(text: String) {
+    var value by remember { mutableStateOf(text) }
+
+    BasicTextField(
+        value = value,
+        onValueChange = { value = it },
+        modifier = Modifier.padding(5.dp),
+        textStyle = TextStyle(
+            fontFamily = FontFamily.Monospace,
+            fontSize = 13.sp
+        )
+    )
+}
+
+@Composable
+fun NodePreview(text: String) {
+    Text(text = text)
+}
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun NodeScreen(node: OrgNode, goBack: () -> Unit) {
     val scrollState = rememberScrollState()
+    var isEditMode by remember { mutableStateOf(false) }
     val context = LocalContext.current
     val fileContent = node.file?.let { readOrgContent(context, it) } ?: "NA"
 
@@ -59,7 +94,29 @@ fun NodeScreen(node: OrgNode, goBack: () -> Unit) {
                     },
                     scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior(
                         rememberTopAppBarState()
-                    )
+                    ),
+                    actions = {
+                        Switch(
+                            checked = isEditMode,
+                            onCheckedChange = { isEditMode = it },
+                            thumbContent = {
+                                if (isEditMode) {
+                                    Icon(
+                                        imageVector = Icons.Filled.Edit,
+                                        contentDescription = null,
+                                        modifier = Modifier.size(SwitchDefaults.IconSize)
+                                    )
+                                } else {
+                                    Icon(
+                                        imageVector = FontAwesomeIcons.Solid.Glasses,
+                                        contentDescription = null,
+                                        modifier = Modifier.size(SwitchDefaults.IconSize)
+                                    )
+                                }
+                            },
+                            modifier = Modifier.padding(horizontal = 20.dp)
+                        )
+                    }
                 )
             },
             floatingActionButton = {
@@ -80,13 +137,17 @@ fun NodeScreen(node: OrgNode, goBack: () -> Unit) {
                 ) {
                     SelectionContainer {
                         Column {
-                            Text(
-                                text = node.title,
-                                fontWeight = FontWeight.Bold,
-                                style = MaterialTheme.typography.headlineLarge,
-                                modifier = Modifier.padding(bottom = 20.dp)
-                            )
-                            Text(text = fileContent, fontFamily = FontFamily.Monospace, fontSize = 13.sp)
+                            if (isEditMode) {
+                                NodeEdit(text = fileContent)
+                            } else {
+                                Text(
+                                    text = node.title,
+                                    fontWeight = FontWeight.Bold,
+                                    style = MaterialTheme.typography.headlineLarge,
+                                    modifier = Modifier.padding(bottom = 20.dp)
+                                )
+                                NodePreview(text = fileContent)
+                            }
                         }
                     }
                 }
