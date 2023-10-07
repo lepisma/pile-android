e7660037222cccb460245e0816bbf18bcf605192
Abhinav Tushar <abhinav@lepisma.xyz>
2023-10-07 21:13:49 +0530

    Add roam ref link button

--- app/src/main/java/com/example/pile/ui/components/Common.kt
+++ app/src/main/java/com/example/pile/ui/components/Common.kt
@@ -1,19 +1,35 @@
 package com.example.pile.ui.components
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.SwitchDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.example.pile.parseOrgBody
+import com.example.pile.parseRoamRef
 import com.example.pile.parseTitle
+import compose.icons.FontAwesomeIcons
+import compose.icons.fontawesomeicons.Solid
+import compose.icons.fontawesomeicons.solid.Bookmark
 
 @Composable
 fun OrgPreview(text: String) {
-    OrgTitle(title = parseTitle(text))
-    OrgBody(text = text)
+    Column {
+        OrgTitle(title = parseTitle(text))
+        OrgRoamRef(text = text)
+        OrgBody(text = text)
+    }
 }
 
 @Composable
@@ -24,6 +40,27 @@ fun OrgTitle(title: String) {
         style = MaterialTheme.typography.headlineLarge,
         modifier = Modifier.padding(bottom = 20.dp)
     )
+}
+
+@Composable
+fun OrgRoamRef(text: String) {
+    val context = LocalContext.current
+
+    parseRoamRef(text)?.let {
+        OutlinedButton(onClick = { println(it) }) {
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(8.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(
+                    imageVector = FontAwesomeIcons.Solid.Bookmark,
+                    contentDescription = "Reference Icon",
+                    modifier = Modifier.size(SwitchDefaults.IconSize)
+                )
+                Text("Reference")
+            }
+        }
+    }
 }
 
 @Composable
