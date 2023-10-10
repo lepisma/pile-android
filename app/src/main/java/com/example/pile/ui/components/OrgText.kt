e8861e8ef00efc6a35e28d471a37f41060f68d5f
Abhinav Tushar <abhinav@lepisma.xyz>
2023-10-10 16:40:25 +0530

    Prettier list bullets

--- app/src/main/java/com/example/pile/ui/components/OrgText.kt
+++ app/src/main/java/com/example/pile/ui/components/OrgText.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.style.TextDecoration
@@ -12,9 +13,32 @@ import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 import com.example.pile.unfillText
 
+fun strikeThrough(original: AnnotatedString): AnnotatedString {
+    return buildAnnotatedString {
+        append(original.text)
+
+        original.spanStyles.forEach { range ->
+            addStyle(range.item, range.start, range.end)
+        }
+
+        addStyle(
+            SpanStyle(textDecoration = TextDecoration.LineThrough),
+            start = 0,
+            end = original.length
+        )
+    }
+}
+
 @Composable
 fun OrgText(text: String, openNode: (String) -> Unit) {
-    val unfilledText = unfillText(text)
+    var unfilledText = unfillText(text)
+    val shouldCross = unfilledText.matches(Regex("(?s)^\\[X\\].*"))
+
+    unfilledText = unfilledText
+        .replace(Regex("^\\[ \\]"), "⬜")
+        .replace(Regex("^\\[-\\]"), "\uD83D\uDD33")
+        .replace(Regex("^\\[X\\]"), "\uD83D\uDFE9")
+
     val nodeLinkPattern = Regex("""\[\[id:([a-fA-F0-9\-]+)\]\[([^\]]+)\]\]""")
     val annotatedString = buildAnnotatedString {
         var lastIndex = 0
@@ -32,7 +56,6 @@ fun OrgText(text: String, openNode: (String) -> Unit) {
             ) {
                 append(label)
             }
-
             addStringAnnotation(
                 tag = "NodeID",
                 annotation = nodeId,
@@ -45,7 +68,7 @@ fun OrgText(text: String, openNode: (String) -> Unit) {
     }
 
     ClickableText(
-        text = annotatedString,
+        text = if (shouldCross) { strikeThrough(annotatedString) } else annotatedString,
         style = MaterialTheme.typography.bodyLarge.copy(color = MaterialTheme.colorScheme.onSurface),
         onClick = { offset ->
             annotatedString.getStringAnnotations("NodeID", offset, offset).firstOrNull()?.let {
