5b77d19380ffcf3f62c6886787d9d8379df0e75e
Abhinav Tushar <abhinav@lepisma.xyz>
2023-10-10 16:09:24 +0530

    Fix horizontal line display

--- app/src/main/java/com/example/pile/ui/components/OrgHorizontalLine.kt
+++ app/src/main/java/com/example/pile/ui/components/OrgHorizontalLine.kt
@@ -1,9 +1,13 @@
 package com.example.pile.ui.components
 
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Divider
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun OrgHorizontalLine() {
-    Text("-----")
+    Divider(color = Color.Gray, thickness = 1.dp, modifier = Modifier.padding(vertical = 15.dp))
 }
