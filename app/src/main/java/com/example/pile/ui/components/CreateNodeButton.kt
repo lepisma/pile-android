1e46ad00a24292dfad76b306744d0298e70fef48
Abhinav Tushar <abhinav@lepisma.xyz>
2024-05-20 10:08:29 +0530

    Remove preview composable

--- app/src/main/java/com/example/pile/ui/components/CreateNodeButton.kt
+++ app/src/main/java/com/example/pile/ui/components/CreateNodeButton.kt
@@ -13,7 +13,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import compose.icons.FontAwesomeIcons
 import compose.icons.fontawesomeicons.Regular
@@ -44,13 +43,5 @@ fun CreateNodeButton(nodeName: String, onClick: (String) -> Unit) {
                 )
             }
         }
-    }
-}
-
-@Preview
-@Composable
-fun test() {
-    CreateNodeButton(nodeName = "Test Node") {
-
     }
 }
