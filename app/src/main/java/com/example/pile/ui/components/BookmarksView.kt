2e503944aed962706f9457d13a2a2dcbe7d4c5aa
Abhinav Tushar <abhinav@lepisma.xyz>
2024-07-09 21:44:01 +0530

    Add search bar for bookmark title match

--- app/src/main/java/com/example/pile/ui/components/BookmarksView.kt
+++ app/src/main/java/com/example/pile/ui/components/BookmarksView.kt
@@ -5,9 +5,13 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedCard
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
@@ -15,18 +19,13 @@ import com.example.pile.OrgNode
 import com.example.pile.isLiteratureNode
 import com.example.pile.isUnsortedNode
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun BookmarksView(nodes: List<OrgNode>, openNode: (OrgNode) -> Unit) {
+    var text by remember { mutableStateOf("") }
+
     Column(modifier = Modifier.padding(horizontal = 20.dp, vertical = 20.dp)) {
         if (nodes.isNotEmpty()) {
-            val randomNodes = remember {
-                nodes
-                    .shuffled()
-                    .filter { isLiteratureNode(it) }
-                    .filter { isUnsortedNode(it) }
-                    .take(5)
-            }
-
             OutlinedCard(
                 modifier = Modifier
                     .fillMaxWidth()
@@ -34,8 +33,20 @@ fun BookmarksView(nodes: List<OrgNode>, openNode: (OrgNode) -> Unit) {
                 border = BorderStroke(0.dp, Color.Transparent),
                 shape = RoundedCornerShape(10.dp)
             ) {
-                NodeList(randomNodes,"Random unsorted bookmarks", openNode, expandedView = true)
+                NodeList(
+                    nodes
+                        .shuffled()
+                        .filter { isLiteratureNode(it) }
+                        .filter { isUnsortedNode(it) }
+                        .filter { text.lowercase() in it.title.lowercase() }
+                        .take(5),
+                    "Random unsorted bookmarks",
+                    openNode,
+                    expandedView = true
+                )
             }
+
+            FindField(text = text, onTextEntry = { text = it }, label = "Find", placeholder = "Node Name")
         }
     }
 }
