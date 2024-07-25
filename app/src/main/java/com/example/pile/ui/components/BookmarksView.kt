e4f8bc7e745d9e69d413ef051dcb822240828d5a
Abhinav Tushar <abhinav@lepisma.xyz>
2024-07-25 23:03:40 +0530

    Add unsorted filter in bookmarksview

--- app/src/main/java/com/example/pile/ui/components/BookmarksView.kt
+++ app/src/main/java/com/example/pile/ui/components/BookmarksView.kt
@@ -3,22 +3,27 @@ package com.example.pile.ui.components
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedCard
+import androidx.compose.material3.Switch
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.example.pile.OrgNode
 import com.example.pile.isLiteratureNode
+import com.example.pile.isUnsortedNode
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -26,6 +31,20 @@ fun BookmarksView(nodes: List<OrgNode>, openNode: (OrgNode) -> Unit) {
     var text by remember { mutableStateOf("") }
 
     val bookmarkNodes = nodes.filter { isLiteratureNode(it) }
+
+    // Filter criteria. We will add categories (via fixed tags) and tags later.
+    var onlyUnsorted by remember { mutableStateOf(true) }
+
+    val filteredNodes = bookmarkNodes
+        .shuffled()
+        .filter { text.lowercase() in it.title.lowercase() }
+        .filter {
+            if (onlyUnsorted) {
+                isUnsortedNode(it)
+            } else {
+                true
+            }
+        }
 
     Column(
         modifier = Modifier
@@ -45,14 +64,19 @@ fun BookmarksView(nodes: List<OrgNode>, openNode: (OrgNode) -> Unit) {
                     shape = RoundedCornerShape(10.dp)
                 ) {
                     NodeList(
-                        bookmarkNodes
-                            .shuffled()
-                            .filter { text.lowercase() in it.title.lowercase() },
-                        "Total ${bookmarkNodes.count()} bookmarks",
+                        filteredNodes,
+                        "${filteredNodes.count()} matches",
                         openNode,
                         expandedView = true
                     )
                 }
+            }
+
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Switch(checked = onlyUnsorted, onCheckedChange = { onlyUnsorted = it })
+                Text("Only unsorted", modifier = Modifier.padding(start = 10.dp))
             }
 
             FindField(text = text, onTextEntry = { text = it }, label = "Find", placeholder = "Node Name")
