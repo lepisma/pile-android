26d3a8e5c662180fbb8eea88a41e1acbf47cd733
Abhinav Tushar <abhinav@lepisma.xyz>
2024-07-26 12:58:34 +0530

    Preserve updated tags in db too
    Earlier this was only done in-memory which was causing inconsistencies while reopening the app

--- app/src/main/java/com/example/pile/viewmodel/SharedViewModel.kt
+++ app/src/main/java/com/example/pile/viewmodel/SharedViewModel.kt
@@ -21,8 +21,14 @@ class SharedViewModel(private val nodeDao: NodeDao, val writeFile: (DocumentFile
     }
 
     fun updateTags(node: OrgNode, tags: List<String>, onCompletion: (OrgNode) -> Unit) {
-        // Tags is not something we maintain in the db, so we just need to update the in-memory
-        // version
-        onCompletion(node.copy(tags = tags))
+        val updatedNode = node.copy(tags = tags)
+
+        viewModelScope.launch(Dispatchers.IO) {
+            nodeDao.updateNode(updatedNode)
+
+            withContext(Dispatchers.Main) {
+                onCompletion(updatedNode)
+            }
+        }
     }
 }
