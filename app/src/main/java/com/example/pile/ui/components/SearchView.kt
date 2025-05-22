6dbc0a9a5b3f7a6f6bb22c9124128d3fb721395d
Abhinav Tushar <abhinav@lepisma.xyz>
2025-05-22 22:45:08 +0530

    Clear search view

--- app/src/main/java/com/example/pile/ui/components/SearchView.kt
+++ app/src/main/java/com/example/pile/ui/components/SearchView.kt
@@ -1,29 +1,8 @@
 package com.example.pile.ui.components
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SearchView() {
-    var text by remember { mutableStateOf("") }
-
-    Column(modifier = Modifier.padding(horizontal = 20.dp, vertical = 20.dp)) {
-        FindField(
-            text = text,
-            onTextEntry = { text = it },
-            label = "Full Text Search",
-            placeholder = "Search String",
-            showButton = true,
-            onButtonClick = { println(it) }
-        )
-    }
 }
