e1328257d8b0698304ed28ff2e1eb9849a414edd
Abhinav Tushar <abhinav@lepisma.xyz>
2025-05-26 14:09:21 +0530

    Add origin in annotatable cards

--- app/src/main/java/com/example/pile/ui/components/mainscreen/home/OrgNodeCard.kt
+++ app/src/main/java/com/example/pile/ui/components/mainscreen/home/OrgNodeCard.kt
@@ -5,6 +5,7 @@ import androidx.annotation.RequiresApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
@@ -19,6 +20,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -37,7 +39,22 @@ import compose.icons.fontawesomeicons.Solid
 import compose.icons.fontawesomeicons.regular.Bookmark
 import compose.icons.fontawesomeicons.regular.Calendar
 import compose.icons.fontawesomeicons.solid.Bookmark
+import java.net.URI
 import kotlin.math.absoluteValue
+
+
+/**
+ * Format node reference for display on cards
+ */
+fun formatRef(ref: String): String? {
+    if (ref.startsWith("http")) {
+        val uri = URI(ref)
+        val domain: String = uri.host
+        return domain.removePrefix("www.")
+    } else {
+        return null
+    }
+}
 
 /**
  * Card for home screen carousels
@@ -85,19 +102,30 @@ fun OrgNodeCard(
                     modifier = Modifier.padding(bottom = 10.dp)
                 ) {
                     if (isLiteratureNode(node)) {
-                        Icon(
-                            imageVector = if (isUnsortedNode(node)) FontAwesomeIcons.Regular.Bookmark else FontAwesomeIcons.Solid.Bookmark,
-                            modifier = Modifier
-                                .size(18.dp)
-                                .padding(end = 5.dp),
-                            contentDescription = "Literature Node",
-                            tint = Color.Gray
-                        )
+                        Row(verticalAlignment = Alignment.CenterVertically) {
+                            Icon(
+                                imageVector = if (isUnsortedNode(node)) FontAwesomeIcons.Regular.Bookmark else FontAwesomeIcons.Solid.Bookmark,
+                                modifier = Modifier
+                                    .size(14.dp)
+                                    .padding(end = 5.dp),
+                                contentDescription = "Literature Node",
+                                tint = Color.Gray
+                            )
+                            val host = formatRef(node.ref!!)
+                            if (host != null) {
+                                Text(
+                                    text = host,
+                                    style = MaterialTheme.typography.bodyMedium,
+                                    color = MaterialTheme.colorScheme.outline,
+                                    modifier = Modifier.padding(start = 5.dp)
+                                )
+                            }
+                        }
                     } else if (isDailyNode(node)) {
                         Icon(
                             imageVector = FontAwesomeIcons.Regular.Calendar,
                             modifier = Modifier
-                                .size(18.dp)
+                                .size(14.dp)
                                 .padding(end = 5.dp),
                             contentDescription = "Daily Node",
                             tint = Color.Gray
