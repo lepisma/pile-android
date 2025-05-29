8531c6207d5a8e501824013b3a8536db191fb567
Abhinav Tushar <abhinav@lepisma.xyz>
2025-05-29 17:52:28 +0530

    Use newer horizontal divider

--- app/src/main/java/com/example/pile/ui/components/orgmode/OrgHorizontalLine.kt
+++ app/src/main/java/com/example/pile/ui/components/orgmode/OrgHorizontalLine.kt
@@ -1,7 +1,7 @@
 package com.example.pile.ui.components.orgmode
 
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Divider
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -9,5 +9,9 @@ import androidx.compose.ui.unit.dp
 
 @Composable
 fun OrgHorizontalLine() {
-    Divider(color = Color.Gray, thickness = 1.dp, modifier = Modifier.padding(vertical = 15.dp))
+    HorizontalDivider(
+        modifier = Modifier.padding(vertical = 15.dp),
+        thickness = 1.dp,
+        color = Color.Gray
+    )
 }
