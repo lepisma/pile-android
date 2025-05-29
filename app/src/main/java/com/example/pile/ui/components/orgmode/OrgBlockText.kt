45a420c8ddc2652cd6235a567aeff281a23b23eb
Abhinav Tushar <abhinav@lepisma.xyz>
2025-05-29 18:09:35 +0530

    Show org block in card

--- app/src/main/java/com/example/pile/ui/components/orgmode/OrgBlockText.kt
+++ app/src/main/java/com/example/pile/ui/components/orgmode/OrgBlockText.kt
@@ -1,6 +1,8 @@
 package com.example.pile.ui.components.orgmode
 
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -11,10 +13,17 @@ import com.example.pile.orgmode.OrgParagraph
 
 @Composable
 fun OrgBlockText(orgBlock: OrgParagraph.OrgBlock) {
-    Text(
-        orgBlock.text,
-        fontFamily = FontFamily.Monospace,
-        fontSize = MaterialTheme.typography.bodyMedium.fontSize,
-        modifier = Modifier.padding(bottom = 10.dp)
-    )
+    ElevatedCard(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 10.dp),
+    ) {
+        Text(
+            orgBlock.text,
+            fontFamily = FontFamily.Monospace,
+            fontSize = MaterialTheme.typography.bodyMedium.fontSize,
+            modifier = Modifier
+                .padding(10.dp)
+        )
+    }
 }
