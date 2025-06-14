b808b796ffa918c48d6df1e119c87ad056e9a08a
Abhinav Tushar <abhinav@lepisma.xyz>
2025-06-14 19:46:49 +0530

    Add folding in heading view

--- app/src/main/java/com/example/pile/ui/components/orgmode/OrgSectionView.kt
+++ app/src/main/java/com/example/pile/ui/components/orgmode/OrgSectionView.kt
@@ -1,17 +1,35 @@
 package com.example.pile.ui.components.orgmode
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.expandVertically
+import androidx.compose.animation.shrinkVertically
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.unit.dp
 import com.example.pile.orgmode.OrgHeading
 import com.example.pile.orgmode.OrgInlineElem
 import com.example.pile.orgmode.OrgSection
+import compose.icons.FontAwesomeIcons
+import compose.icons.fontawesomeicons.Solid
+import compose.icons.fontawesomeicons.solid.ChevronUp
 
 @Composable
-fun OrgSectionHeadingView(heading: OrgHeading, modifier: Modifier = Modifier) {
+fun OrgSectionHeadingView(heading: OrgHeading, isCollapsed: Boolean?, modifier: Modifier = Modifier) {
     val style = when (heading.level.level) {
         1 -> MaterialTheme.typography.displayMedium
         2 -> MaterialTheme.typography.headlineLarge
@@ -19,20 +37,54 @@ fun OrgSectionHeadingView(heading: OrgHeading, modifier: Modifier = Modifier) {
         else -> MaterialTheme.typography.headlineSmall
     }.copy(color = MaterialTheme.colorScheme.onSurface)
 
-    Text(
-        text = heading.title.items
-            .filter { it is OrgInlineElem.Text }
-            .joinToString("") { (it as OrgInlineElem.Text).text },
-        style = style,
-        modifier = modifier.padding(top = 20.dp, bottom = 5.dp)
-    )
+    Row(
+        modifier = modifier.padding(top = 20.dp, bottom = 5.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = heading.title.items
+                .filter { it is OrgInlineElem.Text }
+                .joinToString("") { (it as OrgInlineElem.Text).text },
+            style = style,
+            modifier = Modifier.weight(1f)
+        )
+
+        if (isCollapsed != null) {
+            val rotation by animateFloatAsState(targetValue = if (!isCollapsed) 0f else 180f, label = "arrowRotation")
+
+            Icon(
+                imageVector = FontAwesomeIcons.Solid.ChevronUp,
+                contentDescription = if (isCollapsed) "Expand" else "Collapse",
+                tint = MaterialTheme.colorScheme.outline,
+                modifier = Modifier
+                    .size(24.dp)
+                    .graphicsLayer(rotationZ = rotation)
+                    .padding(start = 10.dp)
+            )
+        }
+    }
 }
 
 @Composable
 fun OrgSectionView(section: OrgSection, modifier: Modifier = Modifier) {
-    OrgSectionHeadingView(section.heading)
+    var isCollapsed by remember { mutableStateOf(true) }
 
-    for (chunk in section.body) {
-        OrgChunkView(chunk, modifier)
+    OrgSectionHeadingView(
+        section.heading,
+        isCollapsed = if (section.body.isNotEmpty()) { isCollapsed } else { null },
+        modifier = Modifier
+            .clickable { isCollapsed = !isCollapsed }
+    )
+
+    AnimatedVisibility(
+        visible = !isCollapsed,
+        enter = expandVertically(),
+        exit = shrinkVertically()
+    ) {
+        Column {
+            for (chunk in section.body) {
+                OrgChunkView(chunk, modifier)
+            }
+        }
     }
 }
